39416d9ccc0d3c8517095b3646dc697328622c99
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-10 20:07:06 -0300

    Implementing loop using WHILE

--- 7193/loop.dart
+++ 7193/loop.dart
@@ -2,4 +2,14 @@ main() {
   for (int x = 0; x < 10; x++) {
     print("Rodou $x vezes!");
   }
+
+  bool condition = true;
+  int x = 0;
+  while (condition) {
+    print("Rodou $x vezes!");
+    if (x > 9) {
+      condition = false;
+    }
+    x++;
+  }
 }
