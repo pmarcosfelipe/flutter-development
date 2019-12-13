8d2416d0a4f06bfee44ec91f4bd2aae7b2d30309
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-13 18:55:06 -0300

    Implemented remove function

--- 7193/shopping-cart.dart
+++ 7193/shopping-cart.dart
@@ -14,6 +14,14 @@ main() {
       for (var i = 0; i < products.length; i++) {
         print("Item $i: ${products[i]} \n");
       }
+    } else if (text == 'remover') {
+      print('Qual item deseja remover?');
+      for (var i = 0; i < products.length; i++) {
+        print("Item $i: ${products[i]} \n");
+      }
+      int item = int.parse(stdin.readLineSync());
+      products.removeAt(item);
+      print('Item removido!');
     } else {
       products.add(text);
       print('\x1B[2J\x1B[0;0H');
