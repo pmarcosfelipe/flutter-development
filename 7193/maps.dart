3ce1253578e1711253a585e7526bb1fafd56f310
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-14 11:53:04 -0300

    Create dynamic map

--- 7193/maps.dart
+++ 7193/maps.dart
@@ -1,10 +1,30 @@
-main() {
-  Map<String, dynamic> maps = {
-    'name': 'Marcos Felipe',
-    'idade': 27,
-    'city': 'Maringa',
-    'state': 'Parana'
-  };
+import 'dart:io';
 
-  print(maps);
+Map<String, dynamic> cadastro = {};
+
+main() {
+  // Map<String, dynamic> maps = {
+  //   'name': 'Marcos Felipe',
+  //   'idade': 27,
+  //   'city': 'Maringa',
+  //   'state': 'Parana'
+  // };
+
+  print('Digite o seu Nome');
+  String name = stdin.readLineSync();
+  cadastro['name'] = name;
+
+  print('Digite a sua Idade');
+  String age = stdin.readLineSync();
+  cadastro['age'] = age;
+
+  print('Digite a sua Cidade');
+  String city = stdin.readLineSync();
+  cadastro['city'] = city;
+
+  print('Digite o seu Estado');
+  String state = stdin.readLineSync();
+  cadastro['state'] = state;
+
+  print(cadastro);
 }
