1420ec169b8baf4d401194b8cd661c82e4aa46c5
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-14 12:40:59 -0300

    Decision structure using arguments

--- 7193/hello_dart/lib/main.dart
+++ 7193/hello_dart/lib/main.dart
@@ -1,3 +1,10 @@
 void main(List<String> arguments) {
   print('Arguments: $arguments');
+  if (arguments[0] == 'estudo') {
+    print('ESTUDO');
+  } else if (arguments[0] == 'imprimir') {
+    print('EXECUTANDO IMPRESSÃO');
+  } else {
+    return;
+  }
 }
