45e84dd78f292f2bca5ea08804b2fe5054490f6c
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-14 13:46:32 -0300

    Constructors

--- 7193/oop_project/lib/main.dart
+++ 7193/oop_project/lib/main.dart
@@ -1,7 +1,8 @@
 import './src/person.dart';
 
 void main(List<String> arguments) {
-  Person person = Person();
+  Person person = Person('Marcos', 27, 'Masculino');
+
   print(person.name);
   print(person.age);
   print(person.gender);
--- 7193/oop_project/lib/src/person.dart
+++ 7193/oop_project/lib/src/person.dart
@@ -2,4 +2,10 @@ class Person {
   String name = 'Marcos';
   int age = 27;
   String gender = 'Masculino';
+
+  Person(String name, int age, String gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
