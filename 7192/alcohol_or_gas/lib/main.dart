7cc28c2e6fd2e8093b2617a0952212f60f602095
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-17 02:32:37 -0300

    Setting up a new project to validate which one is better to use: alcohol or gas

---
+++ 7192/alcohol_or_gas/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alcohol or Gas',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+    );
+  }
+}
