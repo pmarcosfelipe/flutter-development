574b566b3ccc372eb6a2931a5aae8a545dd586b8
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-17 20:50:44 -0300

    Created Widget to Logotipo

--- 7192/alcohol_or_gas/lib/main.dart
+++ 7192/alcohol_or_gas/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:alcohol_or_gas/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -20,7 +21,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-    );
+        backgroundColor: Theme.of(context).primaryColor,
+        body: ListView(children: <Widget>[Logo()]));
   }
 }
