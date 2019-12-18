28cf19b509032a1c92607e4b90eefffe2fd87f76
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-17 21:15:12 -0300

    Created Widget Input and passed label and controller as parameter

--- 7192/alcohol_or_gas/lib/main.dart
+++ 7192/alcohol_or_gas/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:alcohol_or_gas/widgets/input.widget.dart';
 import 'package:alcohol_or_gas/widgets/logo.widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,10 +20,23 @@ class MyApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
+  var _gasCtrl = new MoneyMaskedTextController();
+  var _alcoholCtrl = new MoneyMaskedTextController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).primaryColor,
-        body: ListView(children: <Widget>[Logo()]));
+        body: ListView(children: <Widget>[
+          Logo(),
+          Input(
+            ctrl: _gasCtrl,
+            label: 'Gas',
+          ),
+          Input(
+            ctrl: _alcoholCtrl,
+            label: 'Alcohol',
+          ),
+        ]));
   }
 }
