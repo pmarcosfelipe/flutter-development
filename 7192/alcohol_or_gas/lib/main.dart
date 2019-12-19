6797d09d0a12eb722135bb066703ee46eb8e9479
Marcos Felipe <p.marcosfelipe@gmail.com>
2019-12-18 21:24:31 -0300

    Added button to calcualte

--- 7192/alcohol_or_gas/lib/main.dart
+++ 7192/alcohol_or_gas/lib/main.dart
@@ -37,6 +37,24 @@ class HomePage extends StatelessWidget {
             ctrl: _alcoholCtrl,
             label: 'Alcohol',
           ),
+          Container(
+              margin: EdgeInsets.all(30),
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.8),
+                borderRadius: BorderRadius.circular(60),
+              ),
+              child: FlatButton(
+                child: Text(
+                  'CALCULAR',
+                  style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 25,
+                    fontFamily: 'Big Shoulders Display',
+                  ),
+                ),
+                onPressed: () {},
+              ))
         ]));
   }
 }
